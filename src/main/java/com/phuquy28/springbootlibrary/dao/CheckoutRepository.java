a2d6e70f657b529e74caa401016779de40828efe
phuquy-28 <90885392+phuquy-28@users.noreply.github.com>
2024-05-24 02:22:22 +0700

    back to code

---
+++ src/main/java/com/phuquy28/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,8 @@
+package com.phuquy28.springbootlibrary.dao;
+
+import com.phuquy28.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
